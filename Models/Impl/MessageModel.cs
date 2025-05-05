bea1340853c8a1184bacaa109038dc95347086ce
cozyupk <cozyupk2025@gmail.com>
2025-05-05 11:39:16 +0900

    プロジェクト ファイルを追加します。

---
+++ Models/Impl/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Impl
+{
+    public class MessageModel /* : IMessageModel */
+    {
+        string Message => "Hello from the shadow world!!";
+    }
+
+    public static class AliveCheck
+    {
+        public static string IAmAlive => "Yes";
+    }
+}
