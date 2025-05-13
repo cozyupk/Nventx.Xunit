4c4e948ee5be871445549adee077bb4854efcfa1
cozyupk <cozyupk2025@gmail.com>
2025-05-13 18:18:50 +0900

    Unit evidence files for test coverage were added.

---
+++ src/cli-tools/gen_testcoverage.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+SOLDIR="${DIR}/../"
+
+cd "${SOLDIR}" && dotnet test "HelloShadowDI.sln" --no-build \
+  -p:CollectCoverage=true \
+  -p:CoverletOutput=./coverage.cobertura.xml \
+  -p:CoverletOutputFormat=cobertura
+
+files=""
+
+cd "${SOLDIR}" && \
+for PKG in "DiagnosticPkg"; do
+  for f in `find "${SOLDIR}" -name '*coverage.cobertura.xml' | sed 's@^/\\([a-zA-z]\\)@\\U\\1:@' | sed 's@/@\\\\@g' | grep "$PKG"`; do 
+    if [ -n "${files}" ]; then
+      files="${files};"
+    fi
+    files="${files}$f"
+  done
+done
+
+reportgenerator -reports:$files -targetdir:Documents/utest_coverages/ -reporttypes:Html
